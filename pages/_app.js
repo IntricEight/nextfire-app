870e5df847201cf23a9d51119e69e7b885d647e7
IntricEight <cjbowdoin@gmail.com>
2021-04-28 00:40:43 +0000

    Finished Auth Hook

--- pages/_app.js
+++ pages/_app.js
@@ -3,33 +3,13 @@ import Navbar from '../components/Navbar';
 
 import { Toaster } from 'react-hot-toast';
 import { UserContext } from '../lib/context';
-import { useEffect, useState } from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { firestore } from '../lib/firebase';
+import { useUserData } from '../lib/hooks';
 
 function MyApp({ Component, pageProps }) {
-  const [user] = useAuthState(auth);
-  const [username, setUsername] = useState(null);
-
-  useEffect(() => {
-    let unsubscribe;
-
-    if (user) {
-      const ref = firestore.collection('users').doc(user.uid);
-      unsubscribe = ref.onSnapshot((doc) => {
-        setUsername(doc.data()?.username);
-      })
-      // unsubscribe && unsubscribe();    //For reference
-    } else {
-      setUsername(null);
-    }
-
-  }, [user]);
-
-
+  const userData = useUserData();
+  
   return (
-    // Temporary hardcoding, modify later
-    <UserContext.Provider value={{ user:{}, username: 'collin' }}>
+    <UserContext.Provider value={userData}>
       <Navbar/>
       <Component {...pageProps}/>
       <Toaster/>
